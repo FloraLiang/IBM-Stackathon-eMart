6d35a7a7c443a5ecc60e68530baada860bd6fdbf
Liang Jing <dlliangj@cn.ibm.com>
2020-05-11 13:30:07 +0800

    eMart demo

---
+++ src/app/products.ts
@@ -0,0 +1,27 @@
+export const products = [
+  {
+    name: 'iPhone XR',
+    price: 3000,
+    pic: '../assets/imgs/iphone.jpg',
+    description: 'A large phone with one of the best screens'
+  },
+  {
+    name: 'Huawei P20',
+    price: 1300,
+    pic: '../assets/imgs/Huawei.jpg',
+    description: 'A great phone with one of the best cameras'
+  },
+  {
+    name: 'Redmi Note8',
+    price: 699,
+    pic: '../assets/imgs/Redmi.jpg',
+    description: ''
+  }
+];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
